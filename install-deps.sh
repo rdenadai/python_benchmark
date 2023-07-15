48067d3fded38599df711da2f0122e188c934ed4
Rodolfo De Nadai <rdenadai@gmail.com>
2023-07-15 17:00:47 -0300

    chore: Change install script

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,8 @@
-sudo apt-get update
+#!/bin/bash
+
+sudo apt-get update -y
+
+# Docker
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -7,7 +11,13 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
-sudo apt-get install docker-compose
+sudo apt-get install docker-compose -y
+
+# Python
+sudo apt install python3-pip -y
+alias python="python3"
+python -m pip install --upgrade pip
+python -m pip install packaging
